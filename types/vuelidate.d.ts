852aaf6e2c29dcb3d73187b7ccbf621f0edc368a
wiwiEnnis <ennis.lu@pentium.network>
2020-01-08 11:07:01 +0800

    feature: Extend type definition

--- types/vuelidate.d.ts
+++ types/vuelidate.d.ts
@@ -13,14 +13,19 @@ export interface Validation extends Vue {
     $model: any
     // const validationGetters
     readonly $invalid: boolean
+    readonly $validated: boolean
+    readonly $anyValidated: boolean
     readonly $dirty: boolean
     readonly $anyDirty: boolean
     readonly $error: boolean
     readonly $anyError: boolean
+    readonly $validatedError: boolean
+    readonly $anyValidtedError: boolean
     readonly $pending: boolean
     readonly $params: { [attr: string]: any }
 
     // const validationMethods
+    $validate(): void
     $touch(): void
     $reset(): void
     $flattenParams(): ValidationParams[]
